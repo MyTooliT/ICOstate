944dc48699a9304dcb534dd54eea9620799be6b3
René Schwaiger <sanssecours@me.com>
2025-06-26 12:51:08 +0200

    API: Add first version of stateful class

--- icostate/api.py
+++ icostate/api.py
@@ -1,3 +1,10 @@
 # -- Imports ------------------------------------------------------------------
 
-from icotronic import Connection
+from icotronic.can import Connection
+
+# -- Classes ------------------------------------------------------------------
+
+
+class ICOSystem:
+    def __init__(self):
+        self.connection = Connection()

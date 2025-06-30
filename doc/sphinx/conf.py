6eca4447b240940263dd27500a6740bac6f96938
René Schwaiger <sanssecours@me.com>
2025-06-30 15:23:19 +0200

    Lint: Ignore incorrect warnings

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -4,17 +4,21 @@
 
 # -- Imports ------------------------------------------------------------------
 
+from pathlib import Path
 from datetime import datetime
-from importlib.metadata import version
 
 from sphinx_pyproject import SphinxConfig
 
 # -- Project information ------------------------------------------------------
 
-config = SphinxConfig("../../pyproject.toml", globalns=globals())
-# pylint: disable=redefined-builtin
-copyright = f"{datetime.now().year}, {author}"
-project = name
+config = SphinxConfig(
+    Path(__file__).parent.parent.parent / "pyproject.toml", globalns=globals()
+)
+# pylint: disable=redefined-builtin,undefined-variable
+copyright = f"{datetime.now().year}, {author}"  # type: ignore[name-defined]
+# pylint: enable=redefined-builtin
+project = name  # type: ignore[name-defined]
+# pylint: enable=undefined-variable
 
 # -- General configuration ----------------------------------------------------
 

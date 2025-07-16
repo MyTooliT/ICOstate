fc1e40387d389e970d17be6a91e4b90b48153d0e
René Schwaiger <sanssecours@me.com>
2025-07-16 16:16:10 +0200

    Test: Add event test for rename coroutine

--- test/test_system.py
+++ test/test_system.py
@@ -8,6 +8,11 @@ from pytest import mark
 
 from icostate.system import ICOsystem
 
+# -- Globals ------------------------------------------------------------------
+
+MAC_ADDRESS = "08-6B-D7-01-DE-81"
+NAME = "Test-STH"
+
 # -- Functions ----------------------------------------------------------------
 
 
@@ -30,9 +35,28 @@ async def test_connect():
         mac_address_event_triggered = True
 
     await icosystem.connect_stu()
-    await icosystem.connect_sensor_node_mac("08-6B-D7-01-DE-81")
+    await icosystem.connect_sensor_node_mac(MAC_ADDRESS)
     await sleep(0)  # Allow scheduler to trigger event coroutines
     assert name_event_triggered is True
     assert mac_address_event_triggered is True
     await icosystem.disconnect_sensor_node()
+    await icosystem.disconnect_stu()
+
+
+@mark.asyncio
+async def test_rename():
+    """Test sensor renaming"""
+
+    icosystem = ICOsystem()
+    name_event_triggered = False
+
+    @icosystem.on("sensor_node_name")
+    async def name_changed(data):  # pylint: disable=unused-argument
+        nonlocal name_event_triggered
+        name_event_triggered = True
+
+    await icosystem.connect_stu()
+    await icosystem.rename(NAME, MAC_ADDRESS)
+    await sleep(0)  # Allow scheduler to trigger event coroutines
+    assert name_event_triggered is True
     await icosystem.disconnect_stu()

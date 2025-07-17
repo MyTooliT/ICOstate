f3f4d065fbd47b078c87aaa5ceaf8ede3a28c64f
René Schwaiger <sanssecours@me.com>
2025-07-17 17:02:48 +0200

    Sensor Node Attributes: Add ADC attribute

--- icostate/sensor.py
+++ icostate/sensor.py
@@ -4,6 +4,8 @@
 
 from netaddr import EUI
 
+from icotronic.can.adc import ADCConfiguration
+
 # -- Classes ------------------------------------------------------------------
 
 # pylint: disable=too-few-public-methods
@@ -25,10 +27,14 @@ class SensorNodeAttributes:
     """
 
     def __init__(
-        self, name: str | None = None, mac_address: EUI | None = None
+        self,
+        name: str | None = None,
+        mac_address: EUI | None = None,
+        adc_configuration: ADCConfiguration | None = None,
     ) -> None:
         self.name: str | None = name
         self.mac_address: EUI | None = mac_address
+        self.adc_configuration: ADCConfiguration | None = adc_configuration
 
     def __repr__(self) -> str:
         """Get the textual representation of the sensor node
@@ -39,25 +45,46 @@ class SensorNodeAttributes:
 
         Examples:
 
-            Get representation of a sensor node with defined name & MAC address
+            Get representation sensor node with all attributes defined
 
+
+            >>> config = ADCConfiguration(prescaler=2,
+            ...                           acquisition_time=8,
+            ...                           oversampling_rate=64)
             >>> SensorNodeAttributes(name="hello",
-            ...                      mac_address=EUI("12-34-56-78-90-AB"))
-            Name: hello, MAC Address: 12-34-56-78-90-AB
+            ...                      mac_address=EUI("12-34-56-78-90-AB"),
+            ...                      adc_configuration=config
+            ...                     ) # doctest:+NORMALIZE_WHITESPACE
+            Name: hello,
+            MAC Address: 12-34-56-78-90-AB,
+            ADC Configuration: [Prescaler: 2,
+                                Acquisition Time: 8,
+                                Oversampling Rate: 64,
+                                Reference Voltage: 3.3 V]
 
             Get representation of a sensor node with defined name
 
-            >>> SensorNodeAttributes(name="hello")
-            Name: hello, MAC Address: Undefined
+            >>> SensorNodeAttributes(name="hello"
+            ...                     ) # doctest:+NORMALIZE_WHITESPACE
+            Name: hello,
+            MAC Address: Undefined,
+            ADC Configuration: Undefined
 
         """
 
-        def attribute_or_undefined(attribute):
-            return f"{attribute}" if attribute is not None else "Undefined"
+        def attribute_or_undefined(attribute, brackets: bool = False) -> str:
+            if attribute is None:
+                return "Undefined"
+
+            return f"[{attribute}]" if brackets else str(attribute)
 
         return ", ".join([
             f"Name: {attribute_or_undefined(self.name)}",
             f"MAC Address: {attribute_or_undefined(self.mac_address)}",
+            (
+                "ADC Configuration: "
+                f"{attribute_or_undefined(self.adc_configuration, True)}"
+            ),
         ])
 
 
